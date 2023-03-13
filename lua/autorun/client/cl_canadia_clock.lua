74453db74da6833fe41a2f663d2ee41d28329ab2
JasonMitchell92 <jasonmitchell.jm92@gmail.com>
2023-03-13 11:05:16 -0400

    Update cl_canadia_clock.lua

--- lua/autorun/client/cl_canadia_clock.lua
+++ lua/autorun/client/cl_canadia_clock.lua
@@ -1,40 +1,31 @@
+surface.CreateFont( "Courier_256", { font = "Courier",  size = 256 } )
+surface.CreateFont( "Courier_70", {font = "Courier", size = 70 } )
 
-surface.CreateFont( "Courier_256", {
-	font = "Courier", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
-	extended = false,
-	size = 256,
-	weight = 500,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
-} )
-surface.CreateFont( "Courier_70", {
-	font = "Courier", --  Use the font-name which is shown to you by your operating system Font Viewer, not the file name
-	extended = false,
-	size = 70,
-	weight = 500,
-	blursize = 0,
-	scanlines = 0,
-	antialias = true,
-	underline = false,
-	italic = false,
-	strikeout = false,
-	symbol = false,
-	rotary = false,
-	shadow = false,
-	additive = false,
-	outline = false,
-} )
+local settings = {
+    ["gm_novenka"] = {
+		pos = Vector(1405, 7826, -65),
+		ang = Angle(0,225, 90),
+		scale = 0.275,
+		dist = 8000,
+	},
+	["gm_carcon_ext"] = {
+		pos = Vector(0, -2047,-14300),
+		ang = Angle(0,180,90),
+		scale = 0.275,
+		dist = 4000,
+	}
+}
+if settings[ game.GetMap() ] == nil then return end
 
-setup3D2D("Spawn_Clock", Vector(1405, 7826, -65), Angle(0,225, 90), 8000, 0.275, function()
+local setting = settings[ game.GetMap() ]
+
+setting.dist = setting.dist * setting.dist
+
+hook.Add( "PostDrawOpaqueRenderables", "Canadia_Spawn_Clock", function()
+    if EyePos():DistToSqr( setting.pos ) > setting.dist then return end
+
+    cam.Start3D2D(setting.pos, setting.ang, setting.scale)
+
     surface.SetDrawColor(Color(50,50,50))
 
     local time = os.date('%H:%M')
@@ -49,4 +40,8 @@ setup3D2D("Spawn_Clock", Vector(1405, 7826, -65), Angle(0,225, 90), 8000, 0.275,
 
     surface.DrawRect(-334,118,668,100)
     draw.SimpleText(os.date('%A, %b %d'), "Courier_70", 0, 168, Color(255,255,255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
+
+    cam.End3D2D()
 end)
+
+print("[Canadia] Loaded Clock Screen")
